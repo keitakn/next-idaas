088eeba0e677da2ccf4cacb45ab45ba062cf7c19
keitakn <keita.koga.work@gmail.com>
2020-07-11 18:29:27 +0900

    :tada: #1 Next.jsで開発出来る最低限のコードを追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+  return (
+    <>
+      <h1>Next.js + IDaaS(Identity as a Service)</h1>
+    </>
+  );
+};
+
+export default IndexPage;
