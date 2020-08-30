8105d3973abe2f17e0f3d427ea77514a44aa3272
keitakn <keita.koga.work@gmail.com>
2020-08-30 14:20:19 +0900

    :art: #9 単純なURLエンコードだとメールアドレス内の+も変換されてしまうので以下のように @ のみを置き換えるように変更

--- src/pages/cognito/authorized.tsx
+++ src/pages/cognito/authorized.tsx
@@ -31,7 +31,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   const userName = cookies[userNameKey];
 
-  const idTokenKey = `${keyPrefix}.${encodeURIComponent(userName)}.idToken`;
+  // 単純なURLエンコードだとメールアドレス内の+も変換されてしまうので以下のように @ のみを置き換えるようにする
+  const idTokenKey = `${keyPrefix}.${userName.replace('@', '%40')}.idToken`;
 
   const idToken = cookies[idTokenKey];
 
