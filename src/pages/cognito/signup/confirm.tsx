6d1b8e0d75f462e827617afec19e341fde37fcba
keitakn <keita.koga.work@gmail.com>
2020-12-12 15:58:40 +0900

    :sparkles: #28 https://github.com/keitakn/go-cognito-lambda/issues/33 で追加したカスタム認証を使って登録直後に認証状態にするように変更

--- src/pages/cognito/signup/confirm.tsx
+++ src/pages/cognito/signup/confirm.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
-import { Auth } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
+import { CognitoUser } from 'amazon-cognito-identity-js';
 
 type Props = {
-  user?: { sub: string };
-  error?: Error;
+  user: { sub: string; authenticationToken: string };
+  error: Error;
 };
 
 const Confirm: React.FC<Props> = ({ user, error }: Props) => {
+  useEffect(() => {
+    let unmounted = false;
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    (async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const cognitoUser: CognitoUser = await Auth.signIn(String(user?.sub));
+
+      await Auth.sendCustomChallengeAnswer(
+        cognitoUser,
+        String(user?.authenticationToken),
+      );
+
+      // eslint-disable-next-line no-restricted-globals
+      location.href = 'http://localhost:3900/cognito/authorized';
+    })();
+
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      unmounted = true;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       {error ? <div>エラーが発生しました。 {error.message}</div> : ''}
@@ -17,10 +42,30 @@ const Confirm: React.FC<Props> = ({ user, error }: Props) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  try {
-    const { sub, code } = context.query;
+  Amplify.configure({
+    Auth: {
+      region: process.env.NEXT_PUBLIC_COGNITO_REGION,
+      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+      userPoolWebClientId: process.env.NEXT_PUBLIC_SPA_CLIENT_ID,
+      mandatorySignIn: false,
+      authenticationFlowType: 'CUSTOM_AUTH',
+      cookieStorage: {
+        domain: process.env.NEXT_PUBLIC_AUTH_COOKIE_DOMAIN,
+        path: '/',
+        expires: 7,
+        secure: false,
+      },
+    },
+  });
 
-    if (sub === undefined || code === undefined) {
+  try {
+    const { sub, code, authenticationToken } = context.query;
+
+    if (
+      sub === undefined ||
+      code === undefined ||
+      authenticationToken === undefined
+    ) {
       return {
         props: {},
       };
@@ -29,8 +74,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     // 返り値はSUCCESSという文字列が返ってくるだけ
     await Auth.confirmSignUp(String(sub), String(code));
 
-    return { props: { user: { sub } } };
+    return { props: { user: { sub, authenticationToken } } };
   } catch (e) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     return { props: { error: e } };
   }
 };
